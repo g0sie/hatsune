eac2059546bd1e487a3af646e6b5536c9f944b88
RafalKornel <rafal.kornel123@gmail.com>
2021-08-13 12:21:53 +0200

    Connect frontend to new backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,40 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.requests import Request
+from pydantic import BaseModel
 
 app = FastAPI()
+
+origins = [
+    '*'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class RequestData(BaseModel):
+    text: str
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get('/test')
+def test_echo():
+    return {'data': 'testing'}
+
+
+@app.post('/ping')
+async def ping_echo(data: RequestData):
+    print(data)
+    return data
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,26 +2,51 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import "./App.css";
+import { ChangeEvent } from "rollup";
 
 function App() {
   const [data, setData] = useState("");
+  const [inputText, setInputText] = useState("");
 
   const fetchData = () => {
-    axios
-      .get("http://localhost:8000/test/", {
-        headers: { "Content-Type": "application/json" },
-      })
-      .then((response) => {
-        response.status === 200 && setData(response.data.data);
-      });
+    // fetch("/api/endpoint", {
+    //   method: "POST",
+    //   mode: "cors",
+    //   body: JSON.stringify({ checkboxId: 10 }),
+    // });
+    axios.get("http://localhost:8000/test").then((response) => {
+      response.status === 200 && setData(response.data.data);
+    });
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
+  const sendText = () => {
+    // fetch("http://localhost:8000/ping", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({ text: inputText }),
+    // });
+    axios.post("http://localhost:8000/ping", { text: inputText });
   };
 
   useEffect(fetchData, []);
 
   return (
     <div className="App">
-      <div>data: {data}</div>
-      <button onClick={fetchData}>fetch</button>
+      <div>
+        <div>data: {data}</div>
+        <button onClick={fetchData}>fetch</button>
+      </div>
+
+      <div>
+        <input value={inputText} onChange={handleInputChange} />
+        <button onClick={sendText}>send</button>
+      </div>
     </div>
   );
 }
