df24506db7605ca704846750362a021ebf7beb08
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2021-08-13 12:39:38 +0200

    endpoint with path parameter added

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
+@app.get('/')
+def index():
     return {"Hello": "World"}
+
+@app.get('/numbers/{numbers}')
+def number(numbers: int):
+    return [{'number': number} for number in range(0, numbers + 1)]
