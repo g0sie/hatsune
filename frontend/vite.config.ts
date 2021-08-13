c7b0f7a001581d1411b4f0f1b74d53125c947450
RafalKornel <38111105+RafalKornel@users.noreply.github.com>
2021-08-13 12:42:46 +0200

    Connect frontend to new backend (#14)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        // secure: false,
+        // ws: true,
+      },
+    },
+  },
+});
