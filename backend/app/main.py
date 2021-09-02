6cb0af40acfe10d9360a0d120041e8989510dbd1
Małgorzata Derkacz <77904646+g0sie@users.noreply.github.com>
2021-09-02 19:17:52 +0200

    fastapi debug configurations added (#16)
    * fastapi debug configurations added
    
    * .vscode added to .gitignore
    
    * remove .vscode

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,6 @@ from typing import Optional
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.requests import Request
 from pydantic import BaseModel
 
 app = FastAPI()
